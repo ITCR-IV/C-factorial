e5a9b1d30883b7b4f92360d9cf9f3d1ebd61f22c
Apollo13-0 <calderon.ig08@gmail.com>
2021-04-14 01:13:07 -0600

    GUI complete

--- Mainwindow.cpp
+++ Mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "Mainwindow.h"
 #include "ui_Mainwindow.h"
+#include "qmessagebox.h"
+#include "QFileDialog"
+#include "QFile"
+#include "QTextStream"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +17,78 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QFile file;
+    QTextStream io;
+    QString fileName;
+    fileName = QFileDialog::getOpenFileName(this,"Abrir");
+    file.setFileName(fileName);
+    file.open(QIODevice::ReadOnly|QIODevice::Text);
+    if (!file.isOpen()){
+        QMessageBox::critical(this,"Error", file.errorString());
+        return;
+    }
+    io.setDevice(&file);
+    ui->plainTextEdit->setPlainText(io.readAll());
+    file.flush();
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QFile file;
+    QTextStream io;
+    QString fileName;
+    fileName = QFileDialog::getSaveFileName(this,"Guardar");
+    file.setFileName(fileName);
+    file.open(QIODevice::WriteOnly|QIODevice::Text);
+    if (!file.isOpen()){
+        QMessageBox::critical(this,"Error", file.errorString());
+        return;
+    }
+    io.setDevice(&file);
+    io<<ui->plainTextEdit->toPlainText();
+    file.close();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->plainTextEdit->copy();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->plainTextEdit->cut();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->plainTextEdit->paste();
+}
+
+void MainWindow::on_actionAboutQt_triggered()
+{
+    QMessageBox::aboutQt(this, "Qt");
+}
+
+void MainWindow::on_actionDelete_triggered()
+{
+
+}
+
+void MainWindow::on_actionRun_triggered()
+{
+
+}
+
+void MainWindow::on_actionNext_line_triggered()
+{
+
+}
+
+void MainWindow::on_actionPrev_line_triggered()
+{
+
+}
--- Mainwindow.h
+++ Mainwindow.h
@@ -15,6 +15,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void on_actionRun_triggered();
+
+    void on_actionNext_line_triggered();
+
+    void on_actionPrev_line_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionAboutQt_triggered();
+
+    void on_actionDelete_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
