00559ca9201b836ccc8f93263218a3e598269f3a
Apollo13-0 <calderon.ig08@gmail.com>
2021-04-16 00:40:53 -0600

    add number to code lines not finished

---
+++ LineNumberArea.h
@@ -0,0 +1,27 @@
+#ifndef NUMBERAREA_H
+#define NUMBERAREA_H
+#include <QPlainTextEdit>
+#include "CodeEditor.h"
+
+#endif // QWidget
+
+class LineNumberArea : public QWidget
+{
+public:
+    LineNumberArea(CodeEditor *editor) : QWidget(editor), codeEditor(editor)
+    {}
+
+    QSize sizeHint() const override
+    {
+        return QSize(codeEditor->lineNumberAreaWidth(), 0);
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override
+    {
+        codeEditor->lineNumberAreaPaintEvent(event);
+    }
+
+private:
+    CodeEditor *codeEditor;
+};
