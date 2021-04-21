fdbf1a3b03e22f97bc40b86abd5d752622c4bc4f
Ignacio Vargas <ignaevc@gmail.com>
2021-04-20 20:17:05 -0600

    Defined Token class and changed STRUCT_ID to jsut ID in grammar

--- interpreter/Token.cpp
+++ interpreter/Token.cpp
@@ -1,1 +1,17 @@
 #include "Token.h"
+
+Token::Token(char *type, char *value)
+{
+    this->type = type;
+    this->value = value;
+}
+
+char *Token::getType()
+{
+    return this->type;
+}
+
+char *Token::getValue()
+{
+    return this->value;
+}
--- interpreter/Token.h
+++ interpreter/Token.h
@@ -1,9 +1,19 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
-class Parser
+class Token
 {
+private:
+    char *type;
 
+    char *value;
+
+public:
+    Token(char *type, char *value);
+
+    char *getType();
+
+    char *getValue();
 };
 
 #endif
