43fb4765f9e6d0fa7a425a92be0abff00f95ed85
Ignacio Vargas <ignaevc@gmail.com>
2021-04-21 18:42:35 -0600

    Added constructor declaration and definition

--- interpreter/Parser.cpp
+++ interpreter/Parser.cpp
@@ -1,1 +1,9 @@
 #include "Parser.h"
+#include "Lexer.h"
+
+/*!
+ * \brief Construct a new Parser object and initialize the first token and lexer
+ * 
+ * \param inputLexer must provide a fresh lexer to scan the code with
+ */
+Parser::Parser(Lexer inputLexer) : lexer(inputLexer), currentToken(this->lexer.getNextToken()) {}
