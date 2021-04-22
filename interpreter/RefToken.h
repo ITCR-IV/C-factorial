673fff74ebf4710c50a5b02d05063b566ff64f37
Ignacio Vargas <ignaevc@gmail.com>
2021-04-21 22:11:33 -0600

    Added special Tokens for reference types

---
+++ interpreter/RefToken.h
@@ -0,0 +1,29 @@
+#ifndef REFTOKEN_H
+#define REFTOKEN_H
+
+#include "Token.h"
+#include "TokenConstants.h"
+#include <string>
+
+using namespace std;
+
+class RefToken : public Token
+{
+private:
+    //! the type that the pointer points to
+    string refType;
+
+public:
+    /*!
+     * \brief Construct a new Ref Token object
+     * 
+     * \param refType type that the reference points to
+     * \param value address of pointer
+     */
+    RefToken(string refType, string value) : Token(REFERENCE, value)
+    {
+        this->refType = refType;
+    }
+};
+
+#endif
