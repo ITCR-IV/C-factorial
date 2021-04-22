3dd574c135358615cb049ca342818686c2fb09f5
Ignacio Vargas <ignaevc@gmail.com>
2021-04-22 00:12:07 -0600

    Added singleton implementation

--- ServerManager.cpp
+++ ServerManager.cpp
@@ -1,3 +1,32 @@
 #include "ServerManager.h"
 
-ServerManager::ServerManager(int PORT) : PORT(PORT) {}
+/*!
+ * \brief Constructor for ServerManager, should only be called once due to being a singleton
+ * 
+ * \param PORT where it'll connect with server
+ */
+ServerManager::ServerManager(int PORT)
+{
+    this->PORT = PORT;
+
+    // connect to server here
+}
+
+/*!
+ * \brief method to get (or initialize for the first time) the singleton instance
+ * 
+ * \param PORT where it'll connect with server
+ * \return ServerManager* singleton instance
+ */
+ServerManager *ServerManager::getInstance(int PORT = 9999) //Default port so it doesn't always need one when classes just want to get the singleton and not initialize it
+{
+    /**
+     * This is a safer way to create an instance. instance = new Singleton is
+     * dangeruous in case two instance threads wants to access at the same time
+     */
+    if (singleton == nullptr)
+    {
+        singleton = new ServerManager(PORT);
+    }
+    return singleton;
+}
--- ServerManager.h
+++ ServerManager.h
@@ -5,11 +5,27 @@ class ServerManager
 {
 
 private:
+    //singleton stuff
+    ServerManager(int PORT);
+
+    static ServerManager *singleton;
+
+    ServerManager(ServerManager &other) = delete;
+    void operator=(const ServerManager &) = delete;
+
+    //aquí método para deserealizar json?
+
+    //método general para comunicarse con server, tipo para mandar mensajes y que luego creamos métodos públicos para mandar mensajes específicos
+
 public:
     //! port number to which it connects
     int PORT;
 
-    ServerManager(int PORT);
+    //public singleton stuff
+    ServerManager(ServerManager &other) = delete;   //delete copy constructor (non-cloneable)
+    void operator=(const ServerManager &) = delete; //delete assignment operator (non-assignable)
+
+    static ServerManager *getInstance(int PORT);
 };
 
 #endif
