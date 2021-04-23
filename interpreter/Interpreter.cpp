d8bcb5e7aaec49c11208be6121bb3c7201137ad1
Ignacio Vargas <ignaevc@gmail.com>
2021-04-22 18:56:40 -0600

    Added empty definitions to interpreter methods so code compiles

--- interpreter/Interpreter.cpp
+++ interpreter/Interpreter.cpp
@@ -28,3 +28,27 @@ void Interpreter::error(const string extraDetails = "")
 
     throw RuntimeException(fullMsg);
 }
+
+void Interpreter::enter_scope() {}
+
+void Interpreter::exit_scope() {}
+
+void Interpreter::declaration(string type, string id, string value /*= nullptr*/) {}
+
+void Interpreter::reference_declaration(string ptrType, string id, string value /*= nullptr*/) {}
+
+void Interpreter::enter_struct() {}
+
+void Interpreter::exit_struct(string id) {}
+
+int Interpreter::arithmetic(char operation, int op1, int op2) {}
+
+void *Interpreter::getAddr(string id) {}
+
+string Interpreter::getValue(string id) {}
+
+string Interpreter::getType(string id) {}
+
+void Interpreter::print_call(string msg) {}
+
+string Interpreter::struct_access(string id) {}
