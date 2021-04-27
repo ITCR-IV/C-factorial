9f9bf60b2fd9d3cdbc1baeafb4a44c8f3f844f2a
Ignacio Vargas <ignaevc@gmail.com>
2021-04-27 15:36:52 -0600

    Defined getInfo method

--- server/MServer.cpp
+++ server/MServer.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include "RequestConstants.h"
 #include "JsonDecoder.h"
+#include "JsonEncoder.h"
 #include "TypeConstants.h"
 #include "StructAttribute.h"
 
@@ -247,7 +248,7 @@ void MServer::exit_scope()
 }
 
 /*!
- * \brief Add to memory the variable described by declarationInfo and increase the last entry in declarationsCounter, if inside struct then they don't get stored but assigned to the currentStruct vector as StructAttribute objects and no counter is increased. Lastly, if a defined struct object is being declared the declarationCounter must be increased by as many attributes the struct has +1 (because a variable is stored for the struct itself + its attributes)
+ * \brief Add to memory the variable described by declarationInfo and increase the last entry in declarationsCounter, if inside struct then they don't get stored but assigned to the currentStruct vector as StructAttribute objects and no counter is increased. Lastly, if a defined struct object is being declared the declarationCounter must be increased by as many attributes the struct has +1 (because a variable is stored for the struct itself + its attributes). For declaring a variable without a value, pass the value field as an empty string ("").
  * 
  * \param declarationInfo UpdateInfo where the counter value is ignored and the rest is used to store a new variable in memory
  * \return int -1 for error (most likely the variable is already stored) and 0 for success
@@ -275,36 +276,51 @@ int MServer::declaration(UpdateInfo declarationInfo)
 
         if (type == CHAR)
         {
-            char *c = this->serverMemory + newAddress;
-            *c = value[0];
+            if (value != "")
+            {
+                char *c = this->serverMemory + newAddress;
+                *c = value[0];
+            }
             this->varAddresses.insert({name, newAddress});
             this->varTypes.insert({name, type});
         }
         else if (type == INT)
         {
-            int *i = reinterpret_cast<int *>(this->serverMemory + newAddress);
-            *i = stoi(value);
+            if (value != "")
+            {
+                int *i = reinterpret_cast<int *>(this->serverMemory + newAddress);
+                *i = stoi(value);
+            }
             this->varAddresses.insert({name, newAddress});
             this->varTypes.insert({name, type});
         }
         else if (type == FLOAT)
         {
-            float *f = reinterpret_cast<float *>(this->serverMemory + newAddress);
-            *f = stof(value);
+            if (value != "")
+            {
+                float *f = reinterpret_cast<float *>(this->serverMemory + newAddress);
+                *f = stof(value);
+            }
             this->varAddresses.insert({name, newAddress});
             this->varTypes.insert({name, type});
         }
         else if (type == DOUBLE)
         {
-            double *i = reinterpret_cast<double *>(this->serverMemory + newAddress);
-            *i = stod(value);
+            if (value != "")
+            {
+                double *i = reinterpret_cast<double *>(this->serverMemory + newAddress);
+                *i = stod(value);
+            }
             this->varAddresses.insert({name, newAddress});
             this->varTypes.insert({name, type});
         }
         else if (type == LONG)
         {
-            long *i = reinterpret_cast<long *>(this->serverMemory + newAddress);
-            *i = stol(value);
+            if (value != "")
+            {
+                long *i = reinterpret_cast<long *>(this->serverMemory + newAddress);
+                *i = stol(value);
+            }
             this->varAddresses.insert({name, newAddress});
             this->varTypes.insert({name, type});
         }
@@ -340,7 +356,7 @@ int MServer::declaration(UpdateInfo declarationInfo)
 }
 
 /*!
- * \brief Add to memory the variable described by declarationInfo and increase the last entry in declarationsCounter, if inside struct then they don't get stored but assigned to the currentStruct vector as StructAttribute objects and no counter is increased
+ * \brief Add to memory the variable described by declarationInfo and increase the last entry in declarationsCounter, if inside struct then they don't get stored but assigned to the currentStruct vector as StructAttribute objects and no counter is increased. For declaring a variable without a value, pass the value field as an empty string ("").
  * 
  * \param declarationInfo UpdateInfo where the counter value is ignored and the rest is used to store a new variable in memory, the type is the type the reference points to and has "reference<$type>" wrapped around it when stored
  * \return int -1 for error (most likely the variable is already stored) and 0 for success
@@ -365,8 +381,11 @@ int MServer::reference_declaration(UpdateInfo declarationInfo)
         }
         int newAddress = getAvailableAddress();
 
-        int *i = reinterpret_cast<int *>(this->serverMemory + newAddress); //references are stored in memory with int* because addresses are just that
-        *i = stoi(value);
+        if (value != "")
+        {
+            int *i = reinterpret_cast<int *>(this->serverMemory + newAddress); //references are stored in memory with int* because addresses are just that
+            *i = stoi(value);
+        }
         this->varAddresses.insert({name, newAddress});
 
         std::string refType = "reference<" + type + ">";
@@ -456,16 +475,99 @@ int MServer::exit_struct(std::string id)
 }
 
 /*!
- * \brief Send a variable with type, value, address, and counters info packaged as an UpdateInfo, if it doesn't exist send it with empty fields
+ * \brief Send a variable with type, name, value, address, and counters info packaged as an UpdateInfo json, if it doesn't exist send it with empty fields, structs have an empty value field
  * 
  * \param id name of the variable
  * \return std::string formatted UpdateInfo as json
  */
 std::string MServer::getInfo(std::string id)
 {
-    return "aa";
+    std::string name = id;
+    std::string type;
+    std::string value;
+    int count;
+    int dataAddress;
+
+    // get type
+    type = varTypes.at(id);
+
+    // get address
+    dataAddress = varAddresses.at(id);
+
+    // get value
+    if (type == CHAR)
+    {
+        char *c = this->serverMemory + dataAddress;
+        std::string newValue = std::string(1, *c);
+        value = newValue;
+    }
+    else if (type == INT)
+    {
+        int *i = reinterpret_cast<int *>(this->serverMemory + dataAddress);
+        value = std::to_string(*i);
+    }
+    else if (type == FLOAT)
+    {
+        float *f = reinterpret_cast<float *>(this->serverMemory + dataAddress);
+        value = std::to_string(*f);
+    }
+    else if (type == DOUBLE)
+    {
+        double *d = reinterpret_cast<double *>(this->serverMemory + dataAddress);
+        value = std::to_string(*d);
+    }
+    else if (type == LONG)
+    {
+        long *l = reinterpret_cast<long *>(this->serverMemory + dataAddress);
+        value = std::to_string(*l);
+    }
+    else
+    {
+        //If a struct object is being requested then value == ""
+        value = "";
+    }
+
+    // get count
+
+    map<std::string, int>::iterator p1;
+    count = 0;
+
+    //count how many in varAddresses hold this address (only happens with struct stuff)
+    for (p1 = this->varAddresses.begin(); p1 != this->varAddresses.end(); p1++)
+    {
+        if (p1->second == dataAddress)
+        {
+            count++;
+        }
+    }
+
+    map<std::string, std::string>::iterator p2;
+    //search amongst the reference types the ones pointing to this address
+    for (p2 = this->varTypes.begin(); p2 != this->varTypes.end(); p2++)
+    {
+        if (p2->second.find('<') != std::string::npos) //means we found a reference and now we check if it points to the address
+        {
+            int *r = reinterpret_cast<int *>(this->serverMemory + varAddresses.at(p2->first)); //get an int pointer to the address where the reference value is stored
+            if (*r == dataAddress)
+            { // reference does point to the given address
+                count++;
+            }
+        }
+    }
+
+    //Encode info as JSON
+
+    UpdateInfo info = UpdateInfo(type, name, value, dataAddress, count);
+    JsonEncoder encoder = JsonEncoder(info);
+
+    return encoder.encode();
 }
 
+/*!
+ * \brief Get the last variable assigned in memory, that is, the variable that has the highest address
+ * 
+ * \return std::string the variable's id
+ */
 std::string MServer::getLastVariable()
 {
     std::string lastVarId;
@@ -475,6 +577,11 @@ std::string MServer::getLastVariable()
     return lastVarId;
 }
 
+/*!
+ * \brief Returns the first available address, for this it looks for the last variable in memory and adds to it the amount of bytes its type takes up
+ * 
+ * \return int first available address in memory for allocating new memory
+ */
 int MServer::getAvailableAddress()
 {
     int lastVarAddress = (std::max_element(this->varAddresses.begin(), this->varAddresses.end(), [](const std::pair<std::string, int> &p1, const std::pair<std::string, int> &p2) {
